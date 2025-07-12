e8561fb96811c4fe8de1de673e518f5c8b412bae
Nkem Vincent Nweke <vintio234@gmail.com>
2025-07-13 01:03:25 +0200

    Solution to question 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+""" Import libraries """
+
 import requests
 import sys
 
+"""Gathering data from an API """
+
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    # URLs for API endpoints
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(
-        employee_id
-    )
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
-        employee_id
-    )
-    # Fetch data from API
-    user_info = requests.get(user_url).json()
-    todo_info = requests.get(todo_url).json()
-    # Extract employee information
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+
+    todo = "https://jsonplaceholder.typicode.com/todos?userId={}"
+    todo = todo.format(employee_id)
+
+    user_info = requests.request("GET", url).json()
+
+    todo_info = requests.request("GET", todo).json()
+
     employee_name = user_info.get("name")
-    total_tasks = len(todo_info)
-    # Filter completed tasks
-    completed_tasks = [
-        task for task in todo_info if task.get("completed") is True
-    ]
-    num_completed = len(completed_tasks)
-    # Print results
-    print("Employee {} is done with tasks({}/{}):".format(
-        employee_name, num_completed, total_tasks
-    ))
-    # Print completed task titles with proper formatting
-    for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+    total_tasks = list(filter(lambda x: (x["completed"] is True), todo_info))
+    task_com = len(total_tasks)
+    total_task_done = len(todo_info)
+
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          task_com, total_task_done))
+
+    [print("\t {}".format(task.get("title"))) for task in total_tasks]
+    
