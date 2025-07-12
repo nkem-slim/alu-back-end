e7259056e82e1ec807d30fb42e09f45f58590dfd
Nkem Vincent Nweke <vintio234@gmail.com>
2025-07-12 23:49:21 +0200

    Solution to question 1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-"""Gathering data from an API"""
+"""Gather data from an API"""
 
-import sys
 import requests
+import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    url = (
+        "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    )
+    todo_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+            employee_id
+        )
+    )
 
     user_info = requests.get(url).json()
     todo_info = requests.get(todo_url).json()
 
     employee_name = user_info.get("name")
     total_tasks = len(todo_info)
-    completed_tasks = [task for task in todo_info if task.get("completed") is True]
+    completed_tasks = [
+        task for task in todo_info if task.get("completed") is True
+    ]
     num_completed = len(completed_tasks)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, num_completed, total_tasks))
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name, num_completed, total_tasks
+        )
+    )
     for task in completed_tasks:
-        print("\t {}".format(task.get("title")))
+        print("\t{}".format(task.get("title")))
