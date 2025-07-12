8b26baaf5809e7a612c659ff232e313bd8943aa6
Nkem Vincent Nweke <vintio234@gmail.com>
2025-07-13 01:27:40 +0200

    Solution to question 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,55 +1,51 @@
 #!/usr/bin/python3
 """
-Python script that exports data in the CSV format
+    python script that exports data in the CSV format
 """
 import csv
 import json
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
     """
-    Request user info by employee ID
+        request user info by employee ID
     """
     request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    )
-    
+        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
     """
-    Convert json to dictionary
+        convert json to dictionary
     """
     user = json.loads(request_employee.text)
-    
     """
-    Extract username
+        extract username
     """
     username = user.get("username")
-    
+
     """
-    Request user's TODO list
+        request user's TODO list
     """
     request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1])
-    )
-    
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
     """
-    Convert json to list of dictionaries
+        dictionary to store task status(completed) in boolean format
+    """
+    tasks = {}
+    """
+        convert json to list of dictionaries
     """
     user_todos = json.loads(request_todos.text)
-    
     """
-    Export to CSV
+        loop through dictionary & get completed tasks
     """
-    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
+    for dictionary in user_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
+
+    """
+        export to CSV
+    """
+    with open('{}.csv'.format(argv[1]), mode='w') as file:
         file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        
-        """
-        Loop through todos and write each task as a row
-        """
-        for todo in user_todos:
-            file_editor.writerow([
-                argv[1],  # User ID
-                username,  # Username
-                str(todo.get("completed")).lower(),  # Task status (true/false)
-                todo.get("title")  # Task title
-            ])
+        for k, v in tasks.items():
+            file_editor.writerow([argv[1], username, v, k])
