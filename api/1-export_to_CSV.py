f4dddf6f242340a91d7cde89ffda9f5fb94c6bdf
Nkem Vincent Nweke <vintio234@gmail.com>
2025-07-16 21:29:23 +0200

    Update: solution to question 1 -

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,51 +1,27 @@
 #!/usr/bin/python3
-"""
-    python script that exports data in the CSV format
-"""
-import csv
-import json
-import requests
-from sys import argv
 
+"""This module downloads from an api (jsonplaceholder api)
+and stores it in a csv file"""
+import csv
+import requests
+import sys
 
 if __name__ == "__main__":
-    """
-        request user info by employee ID
-    """
-    request_employee = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}/'.format(argv[1]))
-    """
-        convert json to dictionary
-    """
-    user = json.loads(request_employee.text)
-    """
-        extract username
-    """
-    username = user.get("username")
+    employer_number = sys.argv[1]
+    raw_user_data = requests.get(
+        f"https://jsonplaceholder.typicode.com/users?id={employer_number}")
+    raw_todo_data = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={employer_number}")
+    user_json = raw_user_data.json()
+    todo_json = raw_todo_data.json()
 
-    """
-        request user's TODO list
-    """
-    request_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
-    """
-        dictionary to store task status(completed) in boolean format
-    """
-    tasks = {}
-    """
-        convert json to list of dictionaries
-    """
-    user_todos = json.loads(request_todos.text)
-    """
-        loop through dictionary & get completed tasks
-    """
-    for dictionary in user_todos:
-        tasks.update({dictionary.get("title"): dictionary.get("completed")})
+    username = user_json[0]["username"]
 
-    """
-        export to CSV
-    """
-    with open('{}.csv'.format(argv[1]), mode='w') as file:
-        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for k, v in tasks.items():
-            file_editor.writerow([argv[1], username, v, k])
+    filename = f"{employer_number}.csv"
+    with open(filename, "w", newline="") as file:
+        for todo in todo_json:
+            file.write('"' + str(employer_number) + '",' +
+                       '"' + str(username) + '",' +
+                       '"' + str(todo['completed']) + '",' +
+                       '"' + str(todo['title']) + '",' + '\n'
+                       )
